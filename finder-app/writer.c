b11147a232d357bfcb0faa08a792fc759ddea019
Alexander Safonov <alexander.safonov@promwad.com>
2024-12-31 03:01:37 +0400

    In progress

---
+++ finder-app/writer.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main (int argc, char * argv [])
+{
+	if (argc == 0)
+	{
+		//fprintf(stderr, "No argumets specified. Shall be 2 ones\n");	
+		printf("No argumets specified. Shall be 2 ones\n");	
+	}
+	
+	return 0;
+}
