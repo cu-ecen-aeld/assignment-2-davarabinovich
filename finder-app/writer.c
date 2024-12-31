686a4bfdc9cfff7310fd7e097b85f72a4e80c87c
Alexander Safonov <alexander.safonov@promwad.com>
2024-12-31 18:45:46 +0400

    Logging added

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,26 +1,36 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <syslog.h>
 
 int main (int argc, char * argv [])
 {
+	openlog(NULL, 0, LOG_USER);
 	bool hasFailed = false;
+	
 	if (argc == 1)
 	{
 		fprintf(stderr, "No argumets specified. Shall be 2 ones\n");
+		syslog(LOG_ERR, "No argumets specified. Shall be 2 ones");
 		hasFailed = true;
 	}
 	
 	if (hasFailed)
+	{
+		closelog();
 		return 1;
+	}
 	
 	char * writefile = argv[1];
 	char * writestr = argv[2];
-	
+
+	syslog(LOG_DEBUG, "Writing %s to %s", writestr, writefile);
+		
 	FILE * file_ptr = fopen(writefile, "w");
 	if (file_ptr == NULL)
 	{
-		fprintf(stderr, "Cannot open the file %s", writefile);
+		fprintf(stderr, "Cannot open the file %s\n", writefile);
+		syslog(LOG_ERR, "Cannot open the file %s", writefile);
 		hasFailed = true;	
 	}
 	else
@@ -29,5 +39,6 @@ int main (int argc, char * argv [])
 		fclose(file_ptr);
 	}
 	
-	return hasFailed;
+	closelog();
+	return 0;
 }
