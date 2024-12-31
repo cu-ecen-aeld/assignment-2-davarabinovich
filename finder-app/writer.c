e47ce308847c9c43cf24ea58cee07379e75273c2
Alexander Safonov <alexander.safonov@promwad.com>
2024-12-31 18:28:47 +0400

    Writer.c works.

--- finder-app/writer.c
+++ finder-app/writer.c
@@ -1,13 +1,33 @@
 
+#include <stdbool.h>
 #include <stdio.h>
 
 int main (int argc, char * argv [])
 {
-	if (argc == 0)
+	bool hasFailed = false;
+	if (argc == 1)
 	{
-		//fprintf(stderr, "No argumets specified. Shall be 2 ones\n");	
-		printf("No argumets specified. Shall be 2 ones\n");	
+		fprintf(stderr, "No argumets specified. Shall be 2 ones\n");
+		hasFailed = true;
 	}
 	
-	return 0;
+	if (hasFailed)
+		return 1;
+	
+	char * writefile = argv[1];
+	char * writestr = argv[2];
+	
+	FILE * file_ptr = fopen(writefile, "w");
+	if (file_ptr == NULL)
+	{
+		fprintf(stderr, "Cannot open the file %s", writefile);
+		hasFailed = true;	
+	}
+	else
+	{
+		fputs(writestr, file_ptr);
+		fclose(file_ptr);
+	}
+	
+	return hasFailed;
 }
